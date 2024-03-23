e1fa19cb067747c6ad2dee202ed381d7a0b39712
IgorYarmolchuk <igor.yarmolchuk@gmail.com>
2024-03-23 13:27:46 +0100

    Dokonana naprawa bledow

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -21,7 +21,7 @@ def get_formatted(msg, imie, format):
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "message":' +
+    return ('{ "imie":"' + imie + '", "messege":' +
             msg + '"}')
 
 
@@ -35,5 +35,3 @@ def plain_text_upper_case(msg, imie):
 
 def plain_text_lower_case(msg, imie):
     return plain_text(msg.lower(), imie.lower())
-
-https://github.com/IgorYarmolchuk/2024_L_II_NWh_INFI_K5
